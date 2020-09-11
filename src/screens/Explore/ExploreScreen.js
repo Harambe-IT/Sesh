a1756e1b1e0064ffefe168d02b08ac38549ca632
HairyCat <boris.depooter@harambe.gg>
2020-09-11 18:37:23 +0200

    Started working on explore screen

--- src/screens/Explore/ExploreScreen.js
+++ src/screens/Explore/ExploreScreen.js
@@ -1,12 +1,74 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native';
+import {ScrollView, View, Button, Text} from 'react-native';
 
 import auth from '@react-native-firebase/auth';
 
+import Post from '../../components/Explore/Post';
+
 const ExploreScreen = () => {
+  // Get the posts from the database, placeholder for now
+  const posts = [
+    {
+      id: 1,
+      Owner: {
+        Name: 'Test Name 1',
+        ProfilePictureURL: {uri: 'https://via.placeholder.com/300.png'},
+      },
+      Likes: [{Owner: 'TestLikeOwner', Like: true}],
+      Reactions: [{Owner: 'TestReactionOwner', Content: 'Nice spot'}],
+      Content: {
+        Type: 'Picture',
+        ContentURL: {uri: 'https://picsum.photos/200/300'},
+      },
+    },
+    {
+      id: 2,
+      Owner: {
+        Name: 'Test Name 1',
+        ProfilePictureURL: {uri: 'https://via.placeholder.com/300.png'},
+      },
+      Likes: [{Owner: 'TestLikeOwner', Like: true}],
+      Reactions: [{Owner: 'TestReactionOwner', Content: 'Nice spot'}],
+      Content: {
+        Type: 'Picture',
+        ContentURL: {uri: 'https://picsum.photos/200/300'},
+      },
+    },
+    {
+      id: 3,
+      Owner: {
+        Name: 'Test Name 1',
+        ProfilePictureURL: {uri: 'https://via.placeholder.com/300.png'},
+      },
+      Likes: [{Owner: 'TestLikeOwner', Like: true}],
+      Reactions: [{Owner: 'TestReactionOwner', Content: 'Nice spot'}],
+      Content: {
+        Type: 'Picture',
+        ContentURL: {uri: 'https://picsum.photos/200/300'},
+      },
+    },
+    {
+      id: 4,
+      Owner: {
+        Name: 'Test Name 1',
+        ProfilePictureURL: {uri: 'https://via.placeholder.com/300.png'},
+      },
+      Likes: [{Owner: 'TestLikeOwner', Like: true}],
+      Reactions: [{Owner: 'TestReactionOwner', Content: 'Nice spot'}],
+      Content: {
+        Type: 'Picture',
+        ContentURL: {uri: 'https://picsum.photos/200/300'},
+      },
+    },
+  ];
+
+  const postList = posts.map((post) => {
+    return <Post key={post.id} post={post} />;
+  });
+
   return (
-    <View>
-      <Text>Explore Screen</Text>
+    <ScrollView>
+      <View>{postList}</View>
       <Button
         onPress={() => {
           auth().signOut();
@@ -15,7 +77,7 @@ const ExploreScreen = () => {
         color="#841584"
         accessibilityLabel="Logout using this button"
       />
-    </View>
+    </ScrollView>
   );
 };
 
